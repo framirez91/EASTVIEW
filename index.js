c102c3ffda6d5c4c915998a385b9e2578643b47c
framirez91 <framirez.d85@gmail.com>
2023-11-29 00:13:53 -0300

    Update CORS configuration

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ const cors = require('cors'); // Importa el módulo CORS
 
 app.use(cors()); // Habilita CORS para todas las rutas
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Origin', 'https://aesthetic-jalebi-583fa9.netlify.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
     next();
   });
+
+
 
 // Middleware de body-parser (ya no necesario en versiones recientes de Express)
 app.use(express.urlencoded({ extended: true }));
